90b30a9b48e8e23924989bed60228b011de9657c
Micaela Ceballos <micaelaceballos411@gmail.com>
2022-10-27 05:54:05 -0300

    deadline mode

--- client/src/components/LandingPage/LandingPage.jsx
+++ client/src/components/LandingPage/LandingPage.jsx
@@ -1,16 +1,27 @@
 import React from "react";
 import { NavLink } from "react-router-dom";
 import styles from "./LandingPage.module.css";
+import oceanVideo from "../../video/oceanVideo.mp4.mp4";
 
 export default function LandingPage() {
   return (
     <div className={styles.landingContainer}>
-      <h1 className={styles.landingTitle}>
-        Hi! Are you ready to see the world?
-      </h1>
-      <NavLink to={"/home"} activeClassName={styles.activeHome}>
-        <button className={styles.landingButton}>Click here!</button>
-      </NavLink>
+      <video
+        src={oceanVideo}
+        autoPlay
+        loop
+        muted
+        className={styles.backVideo}
+        type="video/mp4"
+      />
+      <div className={styles.landingContent}>
+        <h1 className={styles.landingTitle}>
+          Hi! Are you ready to explore the world?
+        </h1>
+        <NavLink to={"/home"} activeClassName={styles.activeHome}>
+          <button className={styles.landingButton}>Click here!</button>
+        </NavLink>
+      </div>
     </div>
   );
 }
