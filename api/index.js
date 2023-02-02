4e33ed41226a8042eecf4c5537ae142b067eac84
Micaela Ceballos <micaelaceballos411@gmail.com>
2023-02-02 17:40:51 -0300

    fixing back deploy

--- api/index.js
+++ api/index.js
@@ -19,7 +19,7 @@
 //     ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 const server = require("./src/app.js");
 const { conn } = require("./src/db.js");
-const port = 3001;
+const port = "pi-countries-production-14b6.up.railway.app" || 3001;
 
 // Syncing all the models at once.
 conn.sync({ force: true }).then(() => {
