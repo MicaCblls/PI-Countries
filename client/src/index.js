937499084beec4498a69e4e7dd30383d7366ef89
Micaela Ceballos <micaelaceballos411@gmail.com>
2023-02-02 19:31:35 -0300

    try deploy

--- client/src/index.js
+++ client/src/index.js
@@ -11,7 +11,9 @@ import dotenv from "dotenv";
 
 dotenv.config();
 
-axios.defaults.baseURL = process.env.REACT_APP_API || "http://localhost:3001";
+const { REACT_APP_API } = process.env;
+
+axios.defaults.baseURL = REACT_APP_API || "http://localhost:3001";
 
 ReactDOM.render(
   <Provider store={store}>
