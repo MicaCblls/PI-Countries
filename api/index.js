89d4efb91fa756eda0487842a2c5c490cd50a53f
Micaela Ceballos <micaelaceballos411@gmail.com>
2023-02-02 19:15:50 -0300

    add PORT to index

--- api/index.js
+++ api/index.js
@@ -19,11 +19,13 @@
 //     ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 const server = require("./src/app.js");
 const { conn } = require("./src/db.js");
-const port = 3001;
+require("dotenv").config();
+
+const { PORT } = process.env;
 
 // Syncing all the models at once.
 conn.sync({ force: false }).then(() => {
   server.listen(port, () => {
-    console.log(`%s listening at ${port}`); // eslint-disable-line no-console
+    console.log(`%s listening at ${PORT}`); // eslint-disable-line no-console
   });
 });
